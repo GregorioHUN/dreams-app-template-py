5cf26dc81d6e08bea68c7b99a8425ed3221767a1
romfrolov <justcallmefrolov@gmail.com>
2021-04-22 18:18:19 +0300

    Removes requirements.txt and sets the port

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#       _                              
-#      | |                             
-#    __| |_ __ ___  __ _ _ __ ___  ___ 
+#       _
+#      | |
+#    __| |_ __ ___  __ _ _ __ ___  ___
 #   / _` | '__/ _ \/ _` | '_ ` _ \/ __|
 #  | (_| | | |  __/ (_| | | | | | \__ \
 #   \__,_|_|  \___|\__,_|_| |_| |_|___/ .
@@ -19,18 +19,18 @@ app = Flask(__name__, static_folder='public', template_folder='views')
 # Set the app secret key from the secret environment variables.
 app.secret = os.environ.get('SECRET')
 
-# Dream database. Store dreams in memory for now. 
+# Dream database. Store dreams in memory for now.
 DREAMS = ['Python. Python, everywhere.']
 
 @app.after_request
 def apply_kr_hello(response):
     """Adds some headers to all responses."""
-  
-    # Made by Kenneth Reitz. 
+
+    # Made by Kenneth Reitz.
     if 'MADE_BY' in os.environ:
         response.headers["X-Was-Here"] = os.environ.get('MADE_BY')
-    
-    # Powered by Flask. 
+
+    # Powered by Flask.
     response.headers["X-Powered-By"] = os.environ.get('POWERED_BY')
     return response
 
@@ -39,19 +39,19 @@ def apply_kr_hello(response):
 def homepage():
     """Displays the homepage."""
     return render_template('index.html')
-    
+
 @app.route('/dreams', methods=['GET', 'POST'])
 def dreams():
-    """Simple API endpoint for dreams. 
+    """Simple API endpoint for dreams.
     In memory, ephemeral, like real dreams.
     """
-  
-    # Add a dream to the in-memory database, if given. 
+
+    # Add a dream to the in-memory database, if given.
     if 'dream' in request.args:
         DREAMS.append(request.args['dream'])
-    
-    # Return the list of remembered dreams. 
+
+    # Return the list of remembered dreams.
     return jsonify(DREAMS)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(3000)
